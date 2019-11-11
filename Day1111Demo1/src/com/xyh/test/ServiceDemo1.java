44d89b46981159952ff175d23bb6039c40e07453
xyh <644526466@qq.com>
2019-11-11 15:33:10 +0800

    第二次提交

--- Day1111Demo1/src/com/xyh/test/ServiceDemo1.java
+++ Day1111Demo1/src/com/xyh/test/ServiceDemo1.java
@@ -1,8 +1,18 @@
 package com.xyh.test;
+/**
+ * 关于操作pig的Service接口
+ * @author xyh
+ *
+ */
 
 public interface ServiceDemo1 {
 	/**
 	 * 添加pig的抽象方法
 	 */
 	public abstract void addPig();
+	
+	/**
+	 * 查询pig的抽象方法
+	 */
+	void selectPig();
 }
